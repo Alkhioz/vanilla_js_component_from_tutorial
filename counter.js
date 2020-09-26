febb131c79ee94df7c077ee0ce9d55a5f0f171d0
alkhioz <alemendoza0197@gmail.com>
2020-09-25 23:46:50 -0500

    added html file

--- counter.js
+++ counter.js
@@ -3,11 +3,21 @@ class MyCounter extends HTMLElement{
         super();
         this.shadow = this.attachShadow({mode: "open"});
     }
+
+    static get observedAttributes(){
+        return ["count"];
+    }
+
+    connectedCallback(){
+        this.render();
+    }
+
     render(){//esas comillas alt 96
         this.shadow.innerHTML=`
         <h1>Counter</h1>
         ${this.count}
         <button id='btn'>Increment</button>
-        `
+        `;
     }
 }
+customElements.define('my-counter', MyCounter);
